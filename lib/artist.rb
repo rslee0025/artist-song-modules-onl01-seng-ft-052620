f2c5992a19d1cd9a9ff9667154cd17635e49cf48
Rachel S. Lee <rslee0025@gmail.com>
2020-06-13 19:20:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,11 +28,11 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    songs.each { |song| add_song(song) }
+   
   end
 
 #  def add_songs(songs)
-    
+#     songs.each { |song| add_song(song) }
 #  end
 
 #  def to_param
